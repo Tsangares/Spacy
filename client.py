0034b1db812ea2263903b84be8a10ca04caf2bae
William Wyatt <tsangares@gmail.com>
2020-02-09 21:55:34 -0800

    Updated client for cli

--- client.py
+++ client.py
@@ -1,3 +1,4 @@
+import sys
 import socket
 import pyautogui as gui
 import pynput.keyboard as keyboard
@@ -5,46 +6,60 @@ from time import sleep
 from random import randint
 from multiprocessing import Process, Queue
 from timestamp import timestamp
-index=Queue()
-def listener():
-    HOST,PORT='localhost',6699
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST,PORT))
-        s.sendall(bytes('listening','ascii'))
-        current_index = int(str(s.recv(1024), 'ascii'))
-        while index.qsize() < current_index:
-            index.put('space')
-        print(f'[{timestamp()}] Spacial positions is {index.qsize()} at ')
-        while True:
-            response = int(str(s.recv(1024), 'ascii'))
-            if response > index.qsize():
-                index.put('space')
-                print(f"[{timestamp()}] Recieved instruction to hit space")
-            elif response == index.qsize():
-                print(f"[{timestamp()}] Sucessfully emitted a space request")
-            else:
-                print(f"[{timestamp()}] You have fallen behind by {response-index.qsize()} steps! HOLY FUCK!")
+class Client:
+    def __init__(self,HOST,PORT):
+        self.index=Queue()
+        self.HOST=HOST
+        self.PORT=PORT
+        
+    def run(self):
+        print(f"[{timestamp()}] Connecting to server at {HOST}:{PORT}")
+        l=Process(target=self.listener)
+        s=keyboard.Listener(on_press=self.on_press)
+        l.start()
+        s.start()
+        try:
+            s.join()
+            l.join()
+        except:
+            print("closing")
+            l.terminate()
+        
+    def listener(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((self.HOST,self.PORT))
+            s.sendall(bytes('listening','ascii'))
+            current_index = int(str(s.recv(1024), 'ascii'))
+            while self.index.qsize() < current_index:
+                self.index.put('space')
+                print(f'[{timestamp()}] Spacial positions is {self.index.qsize()} at ')
+            while True:
+                response = int(str(s.recv(1024), 'ascii'))
+                if response > self.index.qsize():
+                    self.index.put('space')
+                    print(f"[{timestamp()}] Recieved instruction to hit space")
+                elif response == self.index.qsize():
+                    print(f"[{timestamp()}] Sucessfully emitted a space request")
+                else:
+                    print(f"[{timestamp()}] You have fallen behind by {response-self.index.qsize()} steps! HOLY FUCK!")
 
-def send_space():
-    HOST,PORT='localhost',6699    
-    index.put('space')
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST,PORT))
-        print(f"[{timestamp()}] Broadcasting a space request")
-        s.sendall(bytes(f'space','ascii'))
+    def send_space(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((self.HOST,self.PORT))
+            print(f"[{timestamp()}] Broadcasting a space request")
+            s.sendall(bytes(f'space','ascii'))
 
-def on_press(key):
-    if key == keyboard.Key.space:
-        send_space()
+    def on_press(self,key):
+        if key == keyboard.Key.space:
+            self.send_space()
         
 if __name__=='__main__':
-    l=Process(target=listener)
-    s=keyboard.Listener(on_press=on_press)
-    l.start()
-    s.start()
-    try:
-        s.join()
-        l.join()
-    except:
-        print("closing")
-        l.terminate()
+    HOST,PORT='localhost',6699
+    if len(sys.argv)>1:
+        HOST=sys.argv[1]
+        if len(HOST.split('.'))==2:
+               HOST,PORT=HOST.split('.')
+    if len(sys.argv)==3:
+        PORT=sys.argv[2]
+    client=Client(HOST,PORT)
+    client.run()
